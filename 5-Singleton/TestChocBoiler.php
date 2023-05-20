c8a2be4bc64b4ac1cd07c9a642b876b30faaabed
Justin Fulton <justin@brightfire.com>
2023-05-20 06:36:01 -0700

    wrap up

--- 5-Singleton/TestChocBoiler.php
+++ 5-Singleton/TestChocBoiler.php
@@ -2,5 +2,21 @@
 
 class TestChocBoiler
 {
+    public function testFillChocBoiler(){
+        echo "Filling the boiler from Testing class.\n" . PHP_EOL;
+        $boiler = ChocolateBoiler::getInstance();
+        $boiler->fill();
+    }
 
+//    public function testDrainChocBoiler(){
+//        echo "Filling the boiler from Testing class.\n" . PHP_EOL;
+//        $boiler = ChocolateBoiler::getInstance();
+//        $boiler->drain();
+//    }
+//
+//    public function testBoilChocBoiler(){
+//        echo "Filling the boiler from Testing class.\n" . PHP_EOL;
+//        $boiler = ChocolateBoiler::getInstance();
+//        $boiler->boil();
+//    }
 }
--- 5-Singleton/index.php
+++ 5-Singleton/index.php
@@ -1,2 +1,20 @@
 <?php
-echo "Hello Singleton";
+require_once './vendor/autoload.php';
+
+//get singleton instance
+$boiler = ChocolateBoiler::getInstance();
+
+//Is the boiler empty
+echo "Is the boiler empty ? => " ;
+var_dump($boiler->isEmpty()) ;
+echo PHP_EOL;
+
+//fill from other class
+(new TestChocBoiler())->testFillChocBoiler();
+
+//check if the single boiler is filled
+echo "Is the boiler empty ? => " ;
+var_dump($boiler->isEmpty()) ;
+echo PHP_EOL;
+
+
