e7535866081b760ee335b45ae21cff62ad2fde3c
Justin Fulton <justin@brightfire.com>
2023-05-27 10:58:50 -0700

    state pattern complete

---
+++ 10-State/states/NoQuarterState.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace States;
+
+use GumballMachine;
+
+class NoQuarterState implements StateInterface
+{
+
+    private GumballMachine $machine;
+
+    public function __construct(GumballMachine $machine)
+    {
+        $this->machine = $machine;
+    }
+
+    public function insertQuarter()
+    {
+        echo "You inserted a quarter.\n";
+
+        $this->machine->setState($this->machine->getHasQuarterState());
+
+    }
+
+    public function ejectQuarter()
+    {
+        echo "You haven't inserted a quarter\n";
+
+    }
+
+    public function turnCrank()
+    {
+        echo "You turned but there's no quarter\n";
+    }
+
+    public function dispense()
+    {
+        echo "You need to pay for your balls\n";
+    }
+
+    public function description(): string
+    {
+        return "has no quarter.";
+    }
+}
